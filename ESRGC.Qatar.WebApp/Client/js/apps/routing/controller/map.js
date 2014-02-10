99a505820e59366b22593e7ec949b45da62d6938
tahoang <dhoang519@gmail.com>
2014-02-10 12:03:13 -0500

    Project setup

---
+++ ESRGC.Qatar.WebApp/Client/js/apps/routing/controller/map.js
@@ -0,0 +1,36 @@
+﻿/*
+Author: Tu hoang
+ESRGC 2014
+
+QATAR
+
+Map controller
+map.js
+
+controller for profile searching by tags
+
+Dependencies
+dx library
+*/
+
+dx.defineController('Map', {
+    refs: {
+
+    },
+    control: {},
+    initialize: function (e) {
+        dx.app.controller.Map.parent.initialize.apply(this, arguments);
+        var scope = this;
+
+        //initialize map
+        var app = dx.getApp();
+        //initialize the map
+        app.appData.mapViewer = new dx.map.LeafletViewer({
+            center: new L.LatLng(25.280468, 51.522312),
+            zoomLevel: 9
+        });
+        app.getMapViewer = function () {
+            return app.appData.mapViewer;
+        };
+    }
+});
---
+++ ESRGC.Qatar.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESRGC.Qatar.WebApp.MvcApplication" Language="C#" %>
