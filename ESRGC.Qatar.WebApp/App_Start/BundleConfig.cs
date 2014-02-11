5ceb5792a1fe95886bf5e98b04dd4008119fd125
tahoang <dhoang519@gmail.com>
2014-02-11 16:52:36 -0500

    Added test map and sample data

--- ESRGC.Qatar.WebApp/App_Start/BundleConfig.cs
+++ ESRGC.Qatar.WebApp/App_Start/BundleConfig.cs
@@ -22,7 +22,10 @@ namespace ESRGC.Qatar.WebApp
       //  bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
       //              "~/Scripts/modernizr-*"));
 
-      //  bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+      //bundles.Add(new StyleBundle("~/style/css").Include(
+      //  "~/client/css/less/site.css",
+      //  "~/client/css/less/map.css"
+      //  ));
 
       //  bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
       //              "~/Content/themes/base/jquery.ui.core.css",
